8cc473e2ba234c67640cc2a96025486160e78277
Giacomo Galliano <g.galliano@list-group.com>
2024-11-25 15:50:15 +0100

    feat: implement search

--- todo-list-frontend/src/app/app.component.ts
+++ todo-list-frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {Todo, TodoService} from "./todo.service";
-import {Observable} from "rxjs";
-import {finalize} from "rxjs/operators";
+import {BehaviorSubject, combineLatest, Observable} from "rxjs";
+import {debounceTime, finalize, map} from "rxjs/operators";
 
 @Component({
     selector: 'app-root',
@@ -13,22 +13,37 @@ import {finalize} from "rxjs/operators";
         </div>
         <div class="list">
             <label for="search">Search...</label>
-            <input id="search" type="text">
+            <input #searchInput id="search" type="text" (input)="onSearchChanged(searchInput.value)">
             <app-progress-bar *ngIf="isLoading"></app-progress-bar>
-            <app-todo-item *ngFor="let todo of todos$ | async" [item]="todo"></app-todo-item>
+            <app-todo-item *ngFor="let todo of filteredTodos$ | async" [item]="todo"></app-todo-item>
         </div>
     `,
     styleUrls: ['app.component.scss']
 })
 export class AppComponent {
 
+    private searchFilter$ = new BehaviorSubject<string>('');
     public isLoading = false;
     readonly todos$: Observable<Todo[]>;
+    readonly filteredTodos$: Observable<Todo[]>;
 
     constructor(todoService: TodoService) {
         this.isLoading = true;
         this.todos$ = todoService.getAll().pipe(
             finalize(() => this.isLoading = false)
         );
+
+        this.filteredTodos$ = combineLatest([
+            this.searchFilter$.pipe(debounceTime(250)),
+            this.todos$
+        ]).pipe(
+            map(([searchFilter, todos]) => {
+                return todos.filter(todo => todo.task.toLowerCase().includes(searchFilter.toLowerCase()))
+            })
+        )
+    }
+
+    onSearchChanged(searchFilter: string): void {
+        this.searchFilter$.next(searchFilter);
     }
 }
