97a77707d4fa1f478488a7720bce87376eac0c84
Giacomo Galliano <g.galliano@list-group.com>
2024-11-25 15:41:37 +0100

    fix: show the loading bar only while data as loading

--- todo-list-frontend/src/app/app.component.ts
+++ todo-list-frontend/src/app/app.component.ts
@@ -1,29 +1,34 @@
 import {Component} from '@angular/core';
 import {Todo, TodoService} from "./todo.service";
 import {Observable} from "rxjs";
+import {finalize} from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <div class="title">
-      <h1>
-        A list of TODOs
-      </h1>
-    </div>
-    <div class="list">
-      <label for="search">Search...</label>
-      <input id="search" type="text">
-      <app-progress-bar></app-progress-bar>
-      <app-todo-item *ngFor="let todo of todos$ | async" [item]="todo"></app-todo-item>
-    </div>
-  `,
-  styleUrls: ['app.component.scss']
+    selector: 'app-root',
+    template: `
+        <div class="title">
+            <h1>
+                A list of TODOs
+            </h1>
+        </div>
+        <div class="list">
+            <label for="search">Search...</label>
+            <input id="search" type="text">
+            <app-progress-bar *ngIf="isLoading"></app-progress-bar>
+            <app-todo-item *ngFor="let todo of todos$ | async" [item]="todo"></app-todo-item>
+        </div>
+    `,
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent {
 
-  readonly todos$: Observable<Todo[]>;
+    public isLoading = false;
+    readonly todos$: Observable<Todo[]>;
 
-  constructor(todoService: TodoService) {
-    this.todos$ = todoService.getAll();
-  }
+    constructor(todoService: TodoService) {
+        this.isLoading = true;
+        this.todos$ = todoService.getAll().pipe(
+            finalize(() => this.isLoading = false)
+        );
+    }
 }
